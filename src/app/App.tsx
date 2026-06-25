5eb09328fcdabbf660c1efee803fb5ecf4e48350
Смирнов Павел Андреевич <p@namoy.ru>
2026-06-25 23:04:02 +0300

    les 2.1
    - router refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss';
-import {Link, Route, Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {classNames} from 'shared/lib/classNames/classNames'
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import AppRouter from "./providers/router/ui/AppRouter";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -16,12 +15,7 @@ const App = () => {
                 <Link to={'/'}>Главная</Link>
                 <Link to={'/about'}>О нас</Link>
             </div>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
