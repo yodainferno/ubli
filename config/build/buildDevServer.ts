33d998a9ac7ba3c775e71207ea296ea601cc1591
Смирнов Павел Андреевич <p@namoy.ru>
2026-06-25 19:22:49 +0300

    les 1.6
    - роутинг
    - асинхронный чанки / lazy loading / code splitting

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,7 @@ import type {Configuration as DevServerConfiguration} from 'webpack-dev-server'
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port,
-        open: true, // авто открывать страницу в браузере
+        open: false, // авто открывать страницу в браузере
+        historyApiFallback: true,
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
-
+import React, {Suspense} from 'react';
 import './index.scss';
+import {Route, Routes, Link} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            Some text
-            <Counter />
+            <div className="nav">
+                <Link to={'/'}>Главная</Link>
+                <Link to={'/about'}>О нас</Link>
+            </div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
