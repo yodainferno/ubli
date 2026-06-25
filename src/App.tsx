ecf441dedec52742c5e25ff4c2126ff94197c1a7
Смирнов Павел Андреевич <p@namoy.ru>
2026-06-25 20:07:10 +0300

    les 1.7
    - themes
    - toggle theme with react context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React, {Suspense} from 'react';
-import './index.scss';
+import React, {Suspense, useContext, useState} from 'react';
+import './styles/index.scss';
 import {Route, Routes, Link} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme"
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
             <div className="nav">
                 <Link to={'/'}>Главная</Link>
                 <Link to={'/about'}>О нас</Link>
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export function someFunc(arg: number): string {
-    console.log('123')
-    return '123'
-}
