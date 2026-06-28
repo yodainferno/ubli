ecfc192fe421667cdbdcc297c97ff0a1b1a3a2f8
Смирнов Павел Андреевич <p@namoy.ru>
2026-06-28 18:07:04 +0300

    les 16

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,5 +6,6 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
         port: options.port,
         open: false, // авто открывать страницу в браузере
         historyApiFallback: true,
+        hot: true,
     }
 }
