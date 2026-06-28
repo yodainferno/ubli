7537831e9c97860a94d81aded4bb9eef6dfb1455
Смирнов Павел Андреевич <p@namoy.ru>
2026-06-28 23:55:56 +0300

    les 20

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,6 +1,10 @@
 type Mods = Record<string, boolean | string>
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(
+    cls: string,
+    mods: Mods | undefined = {},
+    additional: string[] | undefined = [],
+): string {
     return [
         cls,
         ...additional,
