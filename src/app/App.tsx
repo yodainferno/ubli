2addc474e4c584ebb7bee0616db183d2011981b7
Смирнов Павел Андреевич <p@namoy.ru>
2026-06-29 17:46:56 +0300

    les 22

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
@@ -8,6 +8,12 @@ import AppRouter from 'app/providers/router/ui/AppRouter';
 
 const App = () => {
     const { theme } = useTheme();
+
+    // useEffect(() => {
+    //     if (Math.random() < 0.5) {
+    //         throw new Error();
+    //     }
+    // }, []);
 
     return (
         <div className={classNames('app', {}, [theme])}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-// eslint-disable-next-line react/no-deprecated
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
-import App from './app/App';
 
 import 'shared/config/i18n/i18n';
+import { ErrorBoundary } from 'app/providers/ErrorBoudary';
+
+import App from './app/App';
 
 render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
     document.getElementById('root'),
 );
