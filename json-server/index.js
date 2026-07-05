382df0c991a40f0421075877522f4a52b05524e3
Смирнов Павел Андреевич <p@namoy.ru>
2026-07-05 11:05:38 +0300

    les 31.3
    json-server, base auth

---
+++ json-server/index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
+
+const MIN_RESPONSE_TIME = 500;
+const MAX_RESPONSE_TIME = 1200;
+
+// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
+server.use(async (req, res, next) => {
+    const awaitTime = Math.round(
+        MIN_RESPONSE_TIME + Math.random() * (MAX_RESPONSE_TIME - MIN_RESPONSE_TIME),
+    );
+    await new Promise((res) => {
+        setTimeout(res, awaitTime);
+    });
+    next();
+});
+
+// Эндпоинт для логина
+server.post('/login', (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const { users = [] } = db;
+
+        const userFromBd = users.find(
+            (user) => user.username === username && user.password === password,
+        );
+
+        if (userFromBd) {
+            return res.json(userFromBd);
+        }
+
+        return res.status(403).json({ message: 'User not found' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: e.message });
+    }
+});
+
+// проверяем, авторизован ли пользователь
+// eslint-disable-next-line
+server.use((req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ message: 'AUTH ERROR' });
+    }
+
+    next();
+});
+
+server.use(router);
+
+// запуск сервера
+server.listen(8000, () => {
+    console.log('server is running on 8000 port');
+});
