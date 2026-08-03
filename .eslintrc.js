8d7b1214f648cbe07e2c40cd5d115bd5c6af821c
Смирнов Павел Андреевич <p@namoy.ru>
2026-08-03 22:48:25 +0300

    les 64

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
         '@typescript-eslint',
         'i18next',
         'react-hooks',
+        '@yodainferno/ulbi',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -58,6 +59,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
+        '@yodainferno/ulbi/path-checker': 'error',
     },
     globals: {
         __IS_DEV__: true,
